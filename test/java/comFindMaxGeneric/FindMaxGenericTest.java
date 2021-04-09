f0f63e07d18b9f9925ddfd4c849b48884ba0f5a3
kirankumar-Matham96 <mathamkirankumar96@gmail.com>
2021-04-09 13:00:29 +0530

    ADD: Test cases added to test find max method for max number given at first place and second place

--- test/java/comFindMaxGeneric/FindMaxGenericTest.java
+++ test/java/comFindMaxGeneric/FindMaxGenericTest.java
@@ -10,4 +10,11 @@ public class FindMaxGenericTest {
         int max = findMaxGeneric.findMaxNumberAtFirstPosition(5,3,1);
         Assert.assertEquals(5,max);
     }
+    
+    @Test
+    public void givenThreeIntegers_FindsMaxAtSecondPosition_retunsTheSame() {
+        FindMaxGeneric findMaxGeneric = new FindMaxGeneric();
+        int max = findMaxGeneric.findMaxNumberAtFirstPosition(3,5,1);
+        Assert.assertEquals(5,max);
+    }
 }
